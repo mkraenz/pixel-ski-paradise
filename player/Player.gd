661135c1e5df9838930f1ecccf3b152e4becd3ee
Mirco Kraenz <software@kraenz.eu>
2023-04-28 21:33:14 +0200

    feat: sync pole pushing and acceleration

--- player/Player.gd
+++ player/Player.gd
@@ -1,21 +1,24 @@
 extends CharacterBody2D
 
 
-const SPEED = 100.0
-const HORIZONTAL_SPEED = 50.0
-const MAX_VERTICAL_SPEED = 1000.0
-const BRAKE_STRENGTH = 3.0
-const PUSH_STRENGTH = 50.0
-const acceleration_cooldown = 0.7
+const SPEED := 100.0
+const HORIZONTAL_SPEED := 50.0
+const MAX_VERTICAL_SPEED := 1000.0
+const BRAKE_STRENGTH := 3.0
+const PUSH_STRENGTH := 50.0
+const acceleration_cooldown := 0.7
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var anims := $AnimSprite
+@onready var pushPoleAnimTimer := $AnimSprite/PushPoleTimer
 
-var acceleration_timer = acceleration_cooldown + 1.0 # start with cooldown expired
+var acceleration_cooldown_timer := acceleration_cooldown + 1.0 # start with cooldown expired
+var apply_delayed_acceleration = false
 
 func _physics_process(delta):
-	acceleration_timer += delta
+	acceleration_cooldown_timer += delta
 
 	# add gravity to simulate sliding down the slope.
 	velocity.y += gravity * delta
@@ -24,9 +27,14 @@ func _physics_process(delta):
 	if Input.is_action_pressed("brake"):
 		velocity.y = move_toward(velocity.y, 0, BRAKE_STRENGTH)
 		
-	if Input.is_action_just_pressed("accelerate") and acceleration_timer > acceleration_cooldown:
+	if apply_delayed_acceleration:
 		velocity.y = move_toward(velocity.y, MAX_VERTICAL_SPEED, PUSH_STRENGTH)
-		acceleration_timer = 0.0
+		apply_delayed_acceleration = false
+
+	if Input.is_action_just_pressed("accelerate") and acceleration_cooldown_timer > acceleration_cooldown:
+		acceleration_cooldown_timer = 0.0
+		pushPoleAnimTimer.start()
+		anims.play('push_pole')
 
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
@@ -36,3 +44,8 @@ func _physics_process(delta):
 
 	move_and_slide()
 
+
+
+func _on_push_pole_timer_timeout():
+	anims.play('default')
+	apply_delayed_acceleration = true
