aba76c682ec2e895ffb0dafb2e1ff8c8e11855fa
Mirco Kraenz <software@kraenz.eu>
2023-04-28 19:48:50 +0200

    feat: basic acceleration

--- player/Player.gd
+++ player/Player.gd
@@ -5,22 +5,29 @@ const SPEED = 100.0
 const HORIZONTAL_SPEED = 50.0
 const MAX_VERTICAL_SPEED = 1000.0
 const BRAKE_STRENGTH = 3.0
+const PUSH_STRENGTH = 50.0
+const acceleration_cooldown = 0.7
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor(): # if not breaking, add gravity to simulate sliding down the slope.
-		velocity.y += gravity * delta
+var acceleration_timer = acceleration_cooldown + 1.0 # start with cooldown expired
 
-	# Handle Jump.
+func _physics_process(delta):
+	acceleration_timer += delta
+
+	# add gravity to simulate sliding down the slope.
+	velocity.y += gravity * delta
+
+	# if breaking, slow down the vertical speed.
 	if Input.is_action_pressed("brake"):
 		velocity.y = move_toward(velocity.y, 0, BRAKE_STRENGTH)
+		
+	if Input.is_action_just_pressed("accelerate") and acceleration_timer > acceleration_cooldown:
+		velocity.y = move_toward(velocity.y, MAX_VERTICAL_SPEED, PUSH_STRENGTH)
+		acceleration_timer = 0.0
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * HORIZONTAL_SPEED
@@ -28,3 +35,4 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, HORIZONTAL_SPEED)
 
 	move_and_slide()
+
