122c214d2546c2c06e8b3aa1352eed43b1fa6cf5
Mirco Kraenz <software@kraenz.eu>
2023-04-28 22:27:50 +0200

    feat: basic timer

--- ui/TotalTime.gd
+++ ui/TotalTime.gd
@@ -1,15 +1,24 @@
 extends Label
 
 var time_in_secs := 0.0
+var final_time_in_secs := -1.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	time_in_secs += delta
-	var time = snappedf(time_in_secs, 0.001)
+	update_text(time_in_secs)
+	
+func update_text(given_time: float) -> void:
+	var time = snappedf(given_time, 0.001)
 	
 	var millis = fmod(time * 1000, 1000)
 	var seconds = fmod(time, 60)
 	var minutes = fmod(time / 60, 60)
-	
-	# returns a string with the format "MM:SS:MMM"
+	# format "MM:SS:MMM"
 	text = "%02d:%02d:%03d" % [minutes, seconds, millis]
+
+func _on_goal_body_entered(_body) -> void:
+	print("RECEIVED GOAL AREA ENTERED")
+	final_time_in_secs = time_in_secs
+	update_text(final_time_in_secs)
+	set_process(false)
